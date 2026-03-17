b92baf5c1a7165626b983b19563763a2fa0aff1b
Nandhini299 <nandhinni14082005@gmail.com>
2026-03-18 01:48:57 +0530

    Implement second largest number finder
    Added functionality to find the second largest number from a list of integers.

--- Coding.py
+++ Coding.py
@@ -6,3 +6,17 @@ while num > 0:
     rev = rev * 10 + dig
     num //= 10
 print(rev)
+
+# Second Largest number
+num = list(map(int, input().split()))
+
+first = -1 #init min value
+sec = -1
+
+for i in num:
+    if i > first:
+        sec = first
+        first = i
+    elif i > sec:
+        sec = i
+print(sec)
